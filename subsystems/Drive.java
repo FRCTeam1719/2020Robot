aa9915369066fba33370f115f7dd3b6bb93ac4c4
Bennyr20 <Bennyr20@parkschool.net>
2020-01-10 16:22:39 -0500

    worked on drive subsystem

--- subsystems/Drive.java
+++ subsystems/Drive.java
@@ -6,6 +6,9 @@
 /*----------------------------------------------------------------------------*/
 
 package frc.robot.subsystems;
+
+import com.revrobotics.CANSparkMax;
+
 import edu.wpi.first.wpilibj.command.Subsystem;
 import frc.robot.commands.UseDrive;
 
@@ -16,8 +19,50 @@ public class Drive extends Subsystem {
   // Put methods for controlling this subsystem
   // here. Call these from Commands.
 
+  CANSparkMax left1Controller;
+  CANSparkMax left2Controller;
+  CANSparkMax right1Controller;
+  CANSparkMax right2Controller;
+
+  public Drive(CANSparkMax _left1Controller, CANSparkMax _left2Controller, CANSparkMax _right1Controller,
+      CANSparkMax _right2Controller) {
+    super("Drive");
+
+    left1Controller = _left1Controller;
+    left2Controller = _left2Controller;
+    right1Controller = _right1Controller;
+    right2Controller = _right2Controller;
+
+  }
+
   @Override
   public void initDefaultCommand() {
     setDefaultCommand(new UseDrive(this));
+  }
+
+  private double clamp(double val) {
+    if (val < -1)
+      return -1;
+    else if (val > 1)
+      return 1;
+    else
+      return val;
+  }
+
+  void leftSpeed(double speed) {
+    left1Controller.set(speed);
+    left2Controller.set(speed);
+  }
+
+  void rightSpeed(double speed) {
+    right1Controller.set(speed);
+    right2Controller.set(speed);
+  }
+
+  public void drive(double left, double right) {
+
+    // driving without encoders
+    leftSpeed(clamp(left));
+    rightSpeed(clamp(right));
   }
 }
