ec1cc5418403c8e7eb63ee2dccac8c289b82bffc
Amanuel Seifu <Amanuel.Seifu@ad.parkschool.net>
2020-01-10 15:11:52 -0500

    added initial drive setup code

--- commands/UseDrive.java
+++ commands/UseDrive.java
@@ -1,5 +1,5 @@
 /*----------------------------------------------------------------------------*/
-/* Copyright (c) 2019 FIRST. All Rights Reserved.                             */
+/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
 /* Open Source Software - may be modified and shared by FRC teams. The code   */
 /* must be accompanied by the FIRST BSD license file in the root directory of */
 /* the project.                                                               */
@@ -8,15 +8,15 @@
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj.command.Command;
-import frc.robot.Robot;
+import frc.robot.subsystems.Drive;
 
-/**
- * An example command.  You can replace me with your own command.
- */
-public class ExampleCommand extends Command {
-  public ExampleCommand() {
+public class UseDrive extends Command {
+  Drive drive;
+
+  public UseDrive(Drive drive) {
+    this.drive = drive;
     // Use requires() here to declare subsystem dependencies
-    requires(Robot.m_subsystem);
+    // eg. requires(chassis);
   }
 
   // Called just before this Command runs the first time
